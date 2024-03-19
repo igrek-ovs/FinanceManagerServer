7fde6a56132f95397715d6f7143452f8fa052e6b
igrek-ovs <igrekvovcyn@gmail.com>
2024-03-19 14:19:20 +0200

    fixed swagger

--- FinancialAccountingServer/Program.cs
+++ FinancialAccountingServer/Program.cs
@@ -78,11 +78,10 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(builder =>
 {
